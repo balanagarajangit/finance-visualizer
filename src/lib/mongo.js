fdf282832d31a4e577fd901eb158da259bdce4ec
Balanagarajan <54317183+balanagarajangit@users.noreply.github.com>
2025-04-25 20:23:56 +0530

    Update mongo.js

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -3,7 +3,12 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
+let isConnected = false;
+
 export async function connectDB() {
-  if (!client.isConnected) await client.connect();
-  return client.db("financeApp");
+  if (!isConnected) {
+    await client.connect();
+    isConnected = true;
+  }
+  return client.db("financeApp"); 
 }
